586f85b3ef9be248462abb71902982503a7c1549
nizo <martin.niznansky@gmail.com>
2019-12-07 23:35:57 +0100

    refactor, improvements

--- image-compressor.bat
+++ image-compressor.bat
@@ -6,9 +6,16 @@ rem %1 input directory
 rem %2 output directory, defaults to input current
 
 SET /a jpgQuality=75
-SET inputDirectory=%~dp1
+rem SET inputDirectory=%~dp1
 SET inputFileName=%~n1
 
+
+rem extract absolute path of the input directory
+pushd .
+cd %~dp0
+SET inputDirectory=%~f1
+popd
+echo %inputDirectory%
 
 if "%inputDirectory%"=="" (
 	echo No input directory provided
@@ -59,7 +66,7 @@ cd %1
 
 echo.
 
-
+rem echo START %time%
 for /R %%i in (*.jpg *.png) do (
 	if /I "!optimizationStarted!" EQU "0" (
 		echo ==============================
@@ -69,54 +76,51 @@ for /R %%i in (*.jpg *.png) do (
 		SET /a optimizationStarted=1
 	)
 
-
 	SET /a inputFileSize=%%~zi
 	if /I "!overwriteFiles!" EQU "1" (
 		SET outputFile="%%~pi%%~nxi"
 		SET tempFile="%%~pi%%~nxi.__tmp"
-		if /I "%%~xi" EQU ".jpg" (
-			%~dp0/bin/cjpeg-static.exe -quality !jpgQuality! "%%i" > !tempFile!
-		)
-
-		if /I "%%~xi" EQU ".png" (
-			%~dp0/bin/pngquant.exe "%%i" --force --quality=45-85 --output !tempFile!
-		)
-
-		copy /Y !tempFile! !outputFile! >nul
-		del /F !tempFile!
 	) else (
-
 		rem // Save current directory and change to target directory
 		SET currentFileInputDirectory=%%~dpi
 		SET currentFileOutputDirectory=!currentFileInputDirectory:%inputDirectory%=%outputDirectory%!
 		SET outputFile=!currentFileOutputDirectory!%%~nxi
+		SET tempFile=!currentFileOutputDirectory!%%~nxi.__tmp
 
 		if not exist "!currentFileOutputDirectory!" (
 			mkdir !currentFileOutputDirectory!>nul
 		)
-
-		if /I "%%~xi" EQU ".jpg" (
-			%~dp0/bin/cjpeg-static.exe -quality !jpgQuality! "%%i" > !outputFile!
-		)
-
-		if /I "%%~xi" EQU ".png" (
-			%~dp0/bin/pngquant.exe %%i --force --quality=45-85 --output !outputFile!
-		)
 	)
 
-	for %%a in (!outputFile!) do (
-		set /a outputFileSize=%%~za
+	if /I "%%~xi" EQU ".jpg" (
+		%~dp0/bin/cjpeg-static.exe -quality !jpgQuality! "%%i" > !tempFile!
 	)
 
-	SET /a outputSizeTotal=^(!outputSizeTotal!+!outputFileSize!^)
-	SET /a inputSizeTotal=^(!inputSizeTotal!+!inputFileSize!^)
+	if /I "%%~xi" EQU ".png" (
+		%~dp0/bin/pngquant.exe "%%i" --force --quality=45-85 --output !tempFile!
+	)
 
-	SET /a savedInPercent=^(!outputFileSize!*100^)
-	SET /a savedInPercent=^(!savedInPercent!/!inputFileSize!^)
-	SET /a savedInPercent=^(100 - !savedInPercent!^)
+	for %%a in (!tempFile!) do (
+		set /a tempFileSize=%%~za
+	)
 
-	echo !counter!  %%~nxi reduced by ^(!savedInPercent! %%^)
-	set /a counter=^(!counter!+1^)
+	if "!tempFileSize!" NEQ "0" (
+		if "!tempFileSize!" LSS "!inputFileSize!" (
+			copy /Y !tempFile! !outputFile! >nul
+			del /F !tempFile!
+			SET /a outputSizeTotal=^(!outputSizeTotal!+!tempFileSize!^)
+			SET /a inputSizeTotal=^(!inputSizeTotal!+!inputFileSize!^)
+
+			SET /a savedInPercent=^(!tempFileSize!*100^)
+			SET /a savedInPercent=^(!savedInPercent!/!inputFileSize!^)
+			SET /a savedInPercent=^(100 - !savedInPercent!^)
+
+			echo !counter!  %%~nxi reduced by ^(!savedInPercent! %%^)
+			set /a counter=^(!counter!+1^)
+		)
+	) else (
+		echo [101;93mOUTPUT SIZE IS 0 [0m
+	)
 )
 
 if /I "!inputSizeTotal!" NEQ "0" (
@@ -129,6 +133,8 @@ if /I "!inputSizeTotal!" NEQ "0" (
 	echo ==============================
 	echo SIZE REDUCED BY !savedInKB! kB ^(!savedInPercentTotal! %%^)
 	echo ==============================
+
+	rem	echo END %time%
 )
 
 :quit
