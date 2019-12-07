9dc2b1b9d58cd9c989806329487d2d18f02814f4
nizo <martin.niznansky@gmail.com>
2019-12-07 15:10:46 +0100

    improvements, folder cleanup

--- image-compressor.bat
+++ image-compressor.bat
@@ -6,12 +6,15 @@ rem %1 input directory
 rem %2 output directory, defaults to input current
 
 SET /a jpgQuality=75
+SET inputDirectory=%~dp1
+SET inputFileName=%~n1
 
-if "%~1"=="" (
+
+if "%inputDirectory%"=="" (
 	echo No input directory provided
 ) else (
-	if exist "%~1" (
-		if "%~n1" NEQ "" (
+	if exist "%inputDirectory%" (
+		if "%inputFilename%" NEQ "" (
 			echo Single file compression is not supported
 		) else (
 			goto setOutputDirectory
@@ -25,17 +28,18 @@ goto quit
 :setOutputDirectory
 
 if "%~2" NEQ "" (
-	if "!destinationDirectory!" EQU "%~1" (
+	SET outputDirectory=%~dp2
+	if "!outputDirectory!" EQU "!inputDirectory!" (
 		SET /a overwriteFiles=1
 		echo Input and output directories are the same, files will be overwritten
 		SET /P continue=Press enter to continue . . .
 	)
-	if not exist "%~2" (
-		mkdir "%~2"
+	if not exist "!outputDirectory!" (
+		mkdir "!outputDirectory!"
 	)
-	SET destinationDirectory=%~2
+
 ) else (
-	SET destinationDirectory=%~1
+	SET outputDirectory=%inputDirectory%
 	SET overwriteFiles=1
 	echo Output directory is not defined, files in the input directory will be overwritten
 	set /P continue=Press enter to continue . . .
@@ -43,6 +47,7 @@ if "%~2" NEQ "" (
 goto compress
 
 :compress
+
 SET /a inputSizeTotal=0
 SET /a outputSizeTotal=0
 SET /a optimizationStarted=0
@@ -54,6 +59,7 @@ cd %1
 
 echo.
 
+
 for /R %%i in (*.jpg *.png) do (
 	if /I "!optimizationStarted!" EQU "0" (
 		echo ==============================
@@ -63,35 +69,38 @@ for /R %%i in (*.jpg *.png) do (
 		SET /a optimizationStarted=1
 	)
 
+
 	SET /a inputFileSize=%%~zi
 	if /I "!overwriteFiles!" EQU "1" (
 		SET outputFile="%%~pi%%~nxi"
 		SET tempFile="%%~pi%%~nxi.__tmp"
 		if /I "%%~xi" EQU ".jpg" (
-			%~dp0/libs/cjpeg-static.exe -quality !jpgQuality! "%%i" > !tempFile!
+			%~dp0/bin/cjpeg-static.exe -quality !jpgQuality! "%%i" > !tempFile!
 		)
 
 		if /I "%%~xi" EQU ".png" (
-			%~dp0/libs/pngquant.exe "%%i" --force --quality=45-85 --output !tempFile!
+			%~dp0/bin/pngquant.exe "%%i" --force --quality=45-85 --output !tempFile!
 		)
 
 		copy /Y !tempFile! !outputFile! >nul
 		del /F !tempFile!
 	) else (
-		SET outputFile=%%~dpi
-		SET outputFileDirectory=!outputFile:%~1=%~2!
-		SET outputFile=!outputFileDirectory!%%~nxi
 
-		if not exist "!outputFileDirectory!" (
-			mkdir !outputFileDirectory!>nul
+		rem // Save current directory and change to target directory
+		SET currentFileInputDirectory=%%~dpi
+		SET currentFileOutputDirectory=!currentFileInputDirectory:%inputDirectory%=%outputDirectory%!
+		SET outputFile=!currentFileOutputDirectory!%%~nxi
+
+		if not exist "!currentFileOutputDirectory!" (
+			mkdir !currentFileOutputDirectory!>nul
 		)
 
 		if /I "%%~xi" EQU ".jpg" (
-			%~dp0/libs/cjpeg-static.exe -quality !jpgQuality! "%%i" > !outputFile!
+			%~dp0/bin/cjpeg-static.exe -quality !jpgQuality! "%%i" > !outputFile!
 		)
 
 		if /I "%%~xi" EQU ".png" (
-			%~dp0/libs/pngquant.exe %%i --force --quality=45-85 --output !outputFile!
+			%~dp0/bin/pngquant.exe %%i --force --quality=45-85 --output !outputFile!
 		)
 	)
 
